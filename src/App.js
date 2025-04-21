c40f763ca1fabb6d6b1d6a47510fddcc3424350e
Work InTheOffice <41492147-studiovgc1@users.noreply.replit.com>
2025-04-21 11:23:33 +0000

    21.04

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./styles/App.css";
+import PostList from "./components/PostList";
+import PostForm from "./components/PostForm";
+
+function App() {
+  const [posts, setPosts] = useState([
+    { id: 1, title: "Javascript", body: "Description" },
+    { id: 2, title: "Javascript 2", body: "Description" },
+    { id: 3, title: "Javascript 3", body: "Description" },
+  ]);
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  }
+
+  return (
+    <div className="App">
+      <PostForm create={createPost}/>
+      <PostList posts={posts} title={"Posts about JS"} />
+    </div>
+  );
+}
+
+export default App;
