1fe189eff33f014bc27f3a18d40e276e9409e82f
Work InTheOffice <41492147-studiovgc1@users.noreply.replit.com>
2025-04-23 11:12:35 +0000

    sort

--- src/App.js
+++ src/App.js
@@ -2,22 +2,49 @@ import React, { useState } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
+import MySelect from "./components/UI/select/MySelect"
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "Javascript", body: "Description" },
-    { id: 2, title: "Javascript 2", body: "Description" },
-    { id: 3, title: "Javascript 3", body: "Description" },
+    { id: 1, title: "BB", body: "First description" },
+    { id: 2, title: "AA", body: "Second description" },
+    { id: 3, title: "CC", body: "Third description" },
   ]);
+
+  const [selectedSort, setSelectedSort] = useState('');
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   }
 
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id !== post.id));
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <PostList posts={posts} title={"Posts about JS"} />
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sort by"
+          options={[
+            {value: 'title', name: 'By name'},
+            {value: 'body', name: 'By description'},
+          ]}
+          />
+      </div>
+      {posts.length !== 0 
+      ? <PostList posts={posts} remove={removePost} title={"Posts about JS"} /> 
+      : <h1 style={{textAlign: 'center'}}>Posts not found</h1>
+      }
     </div>
   );
 }
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import PostItem from './PostItem';
 
-const PostList = ({posts, title}) => {
+const PostList = ({posts, remove, title}) => {
   return (
     <div>
       <h1 style={{textAlign: 'center'}}>{title}</h1>
-      {posts.map((post, index) => <PostItem number={index+1} post={post} key={post.id}/> )}
+      {posts.map((post, index) => <PostItem number={index+1} post={post} remove={remove} key={post.id}/> )}
     </div>
   );
 }
