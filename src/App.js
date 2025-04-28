85783262975a014b93323271f5c411f6628834b4
Work InTheOffice <41492147-studiovgc1@users.noreply.replit.com>
2025-04-28 10:33:59 +0000

    Axios

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
-import React, { useMemo, useState, createRef } from "react";
+import React, { useState, createRef } from "react";
 import "./styles/App.css";
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
-import MyModal from "./components/UI/MyModal/MyModal"
+import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import { usePosts } from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "BB", body: "First description", nodeRef: createRef(null)},
-    { id: 2, title: "AA", body: "Second description", nodeRef: createRef(null) },
+    { id: 1, title: "BB", body: "First description", nodeRef: createRef(null) },
+    {
+      id: 2,
+      title: "AA",
+      body: "Second description",
+      nodeRef: createRef(null),
+    },
     { id: 3, title: "CC", body: "Third description", nodeRef: createRef(null) },
   ]);
 
-  const [filter, setFilter] = useState({ sort: '', query: '' });
+  const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
+  };
+
+  async function fetchPosts() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts",
+    );
+    console.log(response.data);
   }
 
   const removePost = (post) => {
-    setPosts(posts.filter(p => p.id !== post.id));
-  }
+    setPosts(posts.filter((p) => p.id !== post.id));
+  };
 
   return (
     <div className="App">
-      <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>
+      <MyButton onClick={fetchPosts}>Get posts</MyButton>
+      <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
         Create user
       </MyButton>
-      <MyModal visible={modal} setVisible={setModal}>      
+      <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
       </MyModal>
-      <hr style={{ margin: '15px 0' }} />
-      <PostFilter
-        filter={filter}
-        setFilter={setFilter}
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        posts={sortedAndSearchedPosts}
+        remove={removePost}
+        title={"Posts about JS"}
       />
-      <PostList posts={sortedAndSearchedPosts} remove={removePost} title={"Posts about JS"} />
     </div>
   );
 }
